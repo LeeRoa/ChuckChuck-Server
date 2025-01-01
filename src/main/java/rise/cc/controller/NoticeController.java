d63889b31b62798e0ab4e8f34d0f7bfbeddb18a9
sehoon.gim <sehoongim@gmail.com>
2025-01-01 22:34:37 +0900

    주석 추가한다

--- src/main/java/rise/cc/controller/NoticeController.java
+++ src/main/java/rise/cc/controller/NoticeController.java
@@ -5,6 +5,9 @@ import rise.cc.dto.request.NoticeAddRequest;
 import rise.cc.dto.NoticeAddDto;
 import rise.cc.service.NoticeService;
 
+/**
+ *  공지사항 관련 연산을 처리하는 컨트롤러
+ */
 @RestController
 @RequestMapping("/notice")
 public class NoticeController {
@@ -15,6 +18,10 @@ public class NoticeController {
         this.service = service;
     }
 
+    /**
+     * 사원(계정)을 조회하는 기능
+     * @param request 공지사항에 포함될 내용, 첨부파일(optional)
+     */
     @PostMapping
     public void postNotice(@RequestBody NoticeAddRequest request) {
         service.addNotice(NoticeAddDto.from(request));
