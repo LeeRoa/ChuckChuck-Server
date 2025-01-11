279b65748a03f79afa713192e74ca6f046a8c169
dohyeon <dojin8351@gmail.com>
2025-01-12 01:09:46 +0900

    GlobalExceptionHandler 로그 처리 추가(rise.cc 내, 외부 발생여부 확인후 위치, 메시지 로그 기록), 주석 추가

--- src/main/java/rise/cc/controller/ScheduleController.java
+++ src/main/java/rise/cc/controller/ScheduleController.java
@@ -4,7 +4,6 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import rise.cc.dto.schedule.validation.*;
 import rise.cc.service.ScheduleService;
@@ -20,7 +19,8 @@ public class ScheduleController {
     private final ScheduleService scheduleService;
 
     /**
-     * @param schedule 일정 그룹 생성 요청 (필수 값):
+     * 일정 그룹 생성 요청
+     * @param schedule 필수 값:
      *                 - Integer scheduleGroupName : 생성할 그룹 이름
      *                 - Integer empId : 그룹의 생성자(대표자)
      * @return 성공 실패여부(실패 이유설명)
@@ -31,7 +31,8 @@ public class ScheduleController {
     }
 
     /**
-     @param schedule 일정 생성 요청 데이터 (필수 값):
+     * 일정 생성 요청 데이터
+     @param schedule 필수 값:
       *                 - String scheduleName: 일정 이름
       *                 - Integer scheduleGroupId: 일정 그룹 ID
       *                 - Timestamp scheduleStartDt: 일정 시작 날짜/시간
@@ -45,7 +46,8 @@ public class ScheduleController {
     }
 
     /**
-     * @param scheduleList 생성된 그룹에 구성원 추가 (필수 값):
+     * 생성된 그룹에 구성원 추가
+     * @param scheduleList 필수 값:
      *                 - Integer scheduleGroupId : 구성원을 포함시킬 그룹의 고유번호
      *                 - Integer empId : 추가한 구성원의 고유번호
      * @return 성공 실패여부(실패 이유설명)
@@ -56,17 +58,25 @@ public class ScheduleController {
     }
 
     /**
-     * @param schedule 조회 하고 싶은 일정의 일시, 그룹의 정보를 받아 정보 조회 (필수 값):
+     * 조회 하고 싶은 일정의 일시, 그룹의 정보를 받아 정보 조회
+     * @param schedule 필수 값:
      *                 - Integer schedSuleGroupId : 조회 할 그룹의 고유번호
      *                 - Integer empId : 조회하는 사원의 고유번호
      *                 - Timestamp scheduleDate : 조회하고 싶은 날짜/시간
      * @return 성공 실패여부(실패 이유설명)
      */
-    @GetMapping("")
-    public String scheduleSearch(@Valid ScheduleSearchVDTO schedule) throws JsonProcessingException {
+    @PostMapping("")
+    public String scheduleSearch(@Valid @RequestBody ScheduleSearchVDTO schedule) throws JsonProcessingException {
         return scheduleService.selectSchedule(schedule);
     }
 
+    /**
+     * 일정 삭제 기능
+     * @param schedule 필수 값:
+     *                 - Integer scheduleGroupId : 삭제할 그룹의 고유 번호
+     *                 - empId : 삭제할 그룹의 그룹장 사원의 고유 번호
+     * @return 성공 실패여부(실패 이유설명)
+     */
     @DeleteMapping("/delete")
     public String scheduleDelete(@Valid @RequestBody ScheduleDeleteVDTO schedule) {
         return "";
